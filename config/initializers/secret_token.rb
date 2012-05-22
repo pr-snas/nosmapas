53a74795807de7a23c3b5ca74a64afb47124b4fa
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-22 20:04:07 -0300

    Gerando projeto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nosmapas::Application.config.secret_token = 'b0fa5083f4719856e054564e947121ae5f4d10e0c9811f42070faad3a3328bf5457fece9c61bfe115bd8471e774a5c5de2476d271f97f19a22a139110fed73f2'
